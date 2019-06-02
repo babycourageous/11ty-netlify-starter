f1ae1e78ae456f475eef8096959ca02588efabed
Rene Dellefont <renedellefont@gmail.com>
2019-06-01 22:22:25 -0400

    Add basic 11ty scaffold and scripts

---
+++ .eleventy.js
@@ -0,0 +1,10 @@
+module.exports = function(eleventyConfig) {
+  return {
+    dir: {
+      input: 'src',
+      layouts: '_layouts',
+    },
+    htmlTemplateEngine: 'njk',
+    templateFormats: ['html', 'njk', 'md'],
+  }
+}
