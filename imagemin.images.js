de4f57175f4de18dfe9a4931c94573a4329d374b
Rene Dellefont <renedellefont@gmail.com>
2019-06-03 01:44:23 -0400

    Add image compression stuff

---
+++ imagemin.images.js
@@ -0,0 +1,25 @@
+const imagemin = require('imagemin')
+const imageminMozjpeg = require('imagemin-mozjpeg')
+const imageminPngquant = require('imagemin-pngquant')
+
+const PNGImages = 'src/_assets/images/*.png'
+const JPEGImages = 'src/_assets/images/*.jpg'
+const output = '_site/images'
+
+const optimisePNGImages = () =>
+  imagemin([PNGImages], output, {
+    plugins: [imageminPngquant({ quality: [0.65, 0.8] })],
+  })
+
+const optimiseJPEGImages = () =>
+  imagemin([JPEGImages], output, {
+    plugins: [
+      imageminMozjpeg({
+        quality: 85,
+      }),
+    ],
+  })
+
+optimiseJPEGImages()
+  .then(() => optimisePNGImages())
+  .catch(error => console.log(error))
