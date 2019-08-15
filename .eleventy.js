80171eb7e77e863b6142a8fd625da11de0ac8254
Rene Dellefont <renedellefont@gmail.com>
2019-08-15 17:58:54 -0400

    Update with filters, plugins for sitemap

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,20 @@
+const { DateTime } = require("luxon");
+const pluginRss = require("@11ty/eleventy-plugin-rss");
+
 module.exports = function(eleventyConfig) {
+
+  eleventyConfig.addPlugin(pluginRss);
+
+  // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+  eleventyConfig.addFilter('htmlDateString', (dateObj) => {
+    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LL-dd');
+  });
+
+  // Unsorted items (in whatever order they were added)
+  eleventyConfig.addCollection("all", function(collection) {
+    return collection.getAll();
+  });
+
   return {
     dir: {
       input: 'src',
