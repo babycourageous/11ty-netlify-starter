3754f4930d2cf020b0b399290c1ab9f9656f2ba2
Rene Dellefont <renedellefont@gmail.com>
2019-10-17 17:08:59 -0400

    Update image processing

--- imagemin.images.js
+++ imagemin.images.js
@@ -2,25 +2,22 @@ const imagemin = require('imagemin')
 const imageminMozjpeg = require('imagemin-mozjpeg')
 const imageminPngquant = require('imagemin-pngquant')
 
-const PNGImages = 'src/_assets/images/*.png'
-const JPEGImages = 'src/_assets/images/*.jpg'
 const output = '_site/images'
 
-const optimisePNGImages = () =>
-  imagemin([PNGImages], output, {
-    plugins: [imageminPngquant({ quality: [0.65, 0.8] })],
-  })
-
-const optimiseJPEGImages = () =>
-  imagemin([JPEGImages], output, {
-    plugins: [
-      imageminMozjpeg({
-        quality: 85,
-      }),
-    ],
-  })
-
-optimiseJPEGImages()
-  .then(() => optimisePNGImages())
-  // eslint-disable-next-line no-console
-  .catch(error => console.log(error))
+;(async () => {
+  try {
+    await imagemin(['src/_assets/images/*.{jpg,png}'], {
+      destination: output,
+      plugins: [
+        imageminMozjpeg({
+          quality: 75,
+        }),
+        imageminPngquant({
+          quality: [0.6, 0.8],
+        }),
+      ],
+    })
+  } catch (err) {
+    console.log(err)
+  }
+})()
